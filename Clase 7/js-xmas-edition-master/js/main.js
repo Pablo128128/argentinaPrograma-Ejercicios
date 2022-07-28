9cfdd79787c6d01f428388d9aca917c3ca5ab6b6
Pablo <106037780+Pablo128128@users.noreply.github.com>
2022-07-28 13:31:01 -0300

    Delete Clase 7/js-xmas-edition-master directory

--- Clase 7/js-xmas-edition-master/js/main.js
+++
@@ -1,7 +0,0 @@
-/*
- * Hacer las funciones de validaciÃ³n de validarCiudad y validarDescripcionRegalo.
- * Escribir pruebas para esas funciones.
- *
- * Adicional: Escribir pruebas para las funciones de tareas anteriores.
- *
- * */
